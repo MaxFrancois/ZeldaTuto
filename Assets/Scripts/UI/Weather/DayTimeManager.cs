b33e9451d326551a06876bb3f1cfda5c49afa499
Max <francois.maxime64@gmail.com>
2019-03-30 21:15:25 +1100

    day/night & weather

---
+++ Assets/Scripts/UI/Weather/DayTimeManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayTimeManager : MonoBehaviour
+{
+    public static DayTimeManager instance;
+
+    private bool isGettingBrighter, isGettingDarker, isDayTime, isNightTime;
+    public float TransitionSpeed;
+    //public float dayAndNightDuration = 45f;  // 900 = 15 minutes
+    public float DayTimeDuration;
+    public float NightTimeDuration;
+    private float timeTracker;
+    public float MaxLevelDarkness = 0.75f;
+    private Image darknessImage;
+    public bool toggleLightSource = false;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        darknessImage = GetComponent<Image>();
+        InitializeDayAndNightCycle();
+    }
+
+    void Update()
+    {
+        //ToggleLightSource();
+        DayNightCycle();
+    }
+
+    void InitializeDayAndNightCycle()
+    {
+        isDayTime = true;
+        isNightTime = false;
+        isGettingBrighter = false;
+        isGettingDarker = false;
+        timeTracker = DayTimeDuration;
+    }
+
+    void DayNightCycle()
+    {
+        timeTracker -= Time.deltaTime;
+
+        if (timeTracker <= 0)
+        {
+            if (isDayTime)
+            {
+                isDayTime = false;
+                isGettingDarker = true;
+            }
+            else if (isNightTime)
+            {
+                isNightTime = false;
+                isGettingBrighter = true;
+            }
+        }
+
+        if (isGettingBrighter)
+        {
+            darknessImage.color = new Color(darknessImage.color.r, darknessImage.color.g, darknessImage.color.b,
+                Mathf.MoveTowards(darknessImage.color.a, 0f, TransitionSpeed * Time.deltaTime));
+
+            if (darknessImage.color.a == 0f)
+            {
+                isGettingBrighter = false;
+                isDayTime = true;
+                timeTracker = DayTimeDuration;
+            }
+        }
+
+        if (isGettingDarker)
+        {
+            darknessImage.color = new Color(darknessImage.color.r, darknessImage.color.g, darknessImage.color.b,
+                Mathf.MoveTowards(darknessImage.color.a, MaxLevelDarkness, TransitionSpeed * Time.deltaTime));
+
+            if (darknessImage.color.a >= MaxLevelDarkness)
+            {
+                isGettingDarker = false;
+                isNightTime = true;
+                timeTracker = NightTimeDuration;
+            }
+        }
+    }
+
+    void ToggleLightSource()
+    {
+        //if (toggleLightSource)
+        //{
+        //    if (darknessImage.sprite == nightFull)
+        //    {
+        //        darknessImage.sprite = nightWithLightSource;
+        //    }
+        //    else if (darknessImage.sprite == nightWithLightSource)
+        //    {
+        //        darknessImage.sprite = nightFull;
+        //    }
+        //}
+
+        //toggleLightSource = false;
+    }
+}
---
+++ Assets/Scripts/UI/Weather/WeatherManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public enum WeatherType
+{
+    None,
+    LightRain,
+    HeavyRain,
+    Snow
+}
+
+public enum WeatherAction
+{
+    None,
+    Increase,
+    Decrease
+}
+
+[System.Serializable]
+public class Weather
+{
+    public string Name;
+    public ParticleSystem Particles;
+    public WeatherType Type;
+    public WeatherAction Action;
+    public float MaxIntensity;
+    public float IncreaseIntensitySpeed;
+    public float DecreaseIntensitySpeed;
+}
+
+public class WeatherManager : MonoBehaviour
+{
+    private WeatherType currentWeather;
+    public List<Weather> WeatherOptions;
+    public GameObject FollowTarget;
+    private float currentIncreaseWeatherIntensity;
+    private float currentDecreaseWeatherIntensity;
+    public void SetWeather(float weatherType)
+    {
+        var previousWeather = WeatherOptions.FirstOrDefault(c => c.Type == currentWeather);
+        currentWeather = (WeatherType)weatherType;
+        if (previousWeather != null)
+        {
+            if (previousWeather.Type != WeatherType.None)
+            {
+                previousWeather.Action = WeatherAction.Decrease;
+                currentDecreaseWeatherIntensity = previousWeather.MaxIntensity;
+            }
+            else
+                previousWeather.Action = WeatherAction.None;
+        }
+        var activeWeather = WeatherOptions.First(c => c.Type == currentWeather);
+        if (activeWeather.Type != WeatherType.None)
+        {
+            activeWeather.Particles.gameObject.SetActive(true);
+            activeWeather.Action = WeatherAction.Increase;
+            var emission = activeWeather.Particles.emission;
+            emission.rateOverTime = 0;
+            currentIncreaseWeatherIntensity = 0;
+        }
+        else
+            activeWeather.Action = WeatherAction.None;
+    }
+
+    private void Update()
+    {
+        var newWeather = WeatherOptions.FirstOrDefault(c => c.Action == WeatherAction.Increase);
+        if (newWeather != null)
+        {
+            if (newWeather.MaxIntensity > newWeather.Particles.emission.rateOverTime.constant)
+            {
+                var emission = newWeather.Particles.emission;
+                currentIncreaseWeatherIntensity += Time.deltaTime * newWeather.IncreaseIntensitySpeed;
+                emission.rateOverTime = currentIncreaseWeatherIntensity;
+            }
+            else
+                newWeather.Action = WeatherAction.None;
+        }
+        var oldWeather = WeatherOptions.FirstOrDefault(c => c.Action == WeatherAction.Decrease);
+        if (oldWeather != null)
+        {
+            if (oldWeather.Particles.emission.rateOverTime.constant > 0)
+            {
+                var emission = oldWeather.Particles.emission;
+                currentDecreaseWeatherIntensity -= Time.deltaTime * oldWeather.DecreaseIntensitySpeed;
+                emission.rateOverTime = currentDecreaseWeatherIntensity;
+            }
+            else
+            {
+                oldWeather.Action = WeatherAction.None;
+                StartCoroutine(DisableWeatherCo(oldWeather));
+            }
+        }
+
+        transform.position = FollowTarget.transform.position;
+    }
+
+    private IEnumerator DisableWeatherCo(Weather w)
+    {
+        yield return new WaitForSeconds(5f);
+        w.Particles.gameObject.SetActive(false);
+    }
+}

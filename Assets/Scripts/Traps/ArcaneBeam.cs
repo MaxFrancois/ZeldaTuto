dd6220c3feb041a3411ba8f3d11e456de9d9647f
Max <francois.maxime64@gmail.com>
2019-08-16 23:58:26 +1000

    Laserbeam
    SuperTiled2Unity & MagicArsenal packages

---
+++ Assets/Scripts/Traps/ArcaneBeam.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ArcaneBeam : MonoBehaviour
+{
+
+    [SerializeField] float startDelay;
+    [SerializeField] float laserDuration;
+    [SerializeField] float pauseDuration;
+
+    Animator animator;
+    bool started;
+    bool isFiring;
+    float tracker;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        tracker = pauseDuration;
+        started = false;
+        isFiring = false;
+        StartCoroutine(InitialDelay());
+    }
+
+    IEnumerator InitialDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        started = true;
+    }
+
+    void Update()
+    {
+        if (started)
+        {
+            tracker -= Time.deltaTime;
+            if (tracker <= 0)
+            {
+                isFiring = !isFiring;
+                animator.SetBool("isFiring", isFiring);
+                tracker = isFiring ? laserDuration : pauseDuration;
+            }
+        }
+    }
+}
